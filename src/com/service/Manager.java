04466974c3de4098409cc508e629a583514934f8
andresinho20049 <>
2017-12-21 21:48:32 -0200

    Segundo Commit, criando instancia para EntityManager

---
+++ src/com/service/Manager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author andre
+ */
+public class Manager {
+    
+    private EntityManager em;
+    
+    private static Manager instance = null;
+    
+    public Manager getInstance(){
+        if (instance==null){
+            instance = new Manager();
+        }
+        return instance;
+    }
+    
+    private Manager(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("LojaCrudHibernatePU");
+        em = emf.createEntityManager();
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    
+}
