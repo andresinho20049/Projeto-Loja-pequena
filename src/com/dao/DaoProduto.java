d7f86011add1f69284f7c1cea39dcbbc1e25b36c
andresinho20049 <>
2017-12-21 22:08:23 -0200

    Terceiro Commit, classe dao completa.

--- src/com/dao/DaoProduto.java
+++ src/com/dao/DaoProduto.java
@@ -6,7 +6,10 @@
 package com.dao;
 
 import com.model.Produto;
+import com.service.Manager;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  *
@@ -14,18 +17,31 @@ import java.util.List;
  */
 public class DaoProduto {
     
+    private EntityManager em;
+    
+    public DaoProduto(){
+        em = Manager.getInstance().getEm();
+    }
+    
     public void Salvar(Produto p){
-        
+        em.getTransaction().begin();
+        if (p.getCodigo()>0){
+            p = em.merge(p);
+        }
+        em.persist(p);
+        em.getTransaction().commit();
     }
     
     public List<Produto> findAll(){
-        
-    
-    return null;
+        Query qry = em.createQuery("Select p from com.model.Produto p ");
+        List result = qry.getResultList();  
+        return (List<Produto>) result;
     }
     
     public void Remover(Produto p){
-        
+        em.getTransaction().begin();
+        em.remove(p);
+        em.getTransaction().commit();
     }
     
 }
